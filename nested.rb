88feba4354f456953e0c493a3af3cdf57926a301
Antonio Blandin <antonio.blandin25@gmail.com>
2019-12-01 05:08:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for] = "COBOL"
+     programmer_hash[:alan_kay][:languages] = ["COBOL", "FORTRAN"]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for] = "COBOL"
+     programmer_hash[:alan_kay][:languages] = ["COBOL", "FORTRAN"]
 end
 
 def adding_matz
